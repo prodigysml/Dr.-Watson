37eaf6666599ff41db1b9911b98a87f4f22f9141
prodigysml <saj.lohani@gmail.com>
2019-10-31 16:39:21 +1100

    Added in automatic checking for any updates. User still needs to download updates themselves

--- __version__.py
+++ __version__.py
@@ -1,1 +1,11 @@
+import urllib2
+
 __version__ = "1.0.1"
+
+try:
+    
+    r = urllib2.urlopen("https://raw.githubusercontent.com/prodigysml/Dr.-Watson/master/__version__.py", timeout=3)
+    if not __version__ == r.read().split('"')[1]:
+        print("Dr. Watson is out of date. Please download the latest version from GitHub. https://prodigysml/Dr.-Watson/")
+except:
+    pass
