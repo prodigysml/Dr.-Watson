984e0f55b018f07b86d7df0c48440a2f74b540e0
prodigysml <saj.lohani@gmail.com>
2019-10-31 16:51:20 +1100

    Upon multiple users request, we are removing this feature.

--- __version__.py
+++ __version__.py
@@ -1,11 +1,1 @@
-import urllib2
-
-__version__ = "1.1.0"
-
-try:
-    
-    r = urllib2.urlopen("https://raw.githubusercontent.com/prodigysml/Dr.-Watson/master/__version__.py", timeout=3)
-    if not __version__ == r.read().split('"')[1]:
-        print("Dr. Watson is out of date. Please download the latest version from GitHub. https://prodigysml/Dr.-Watson/")
-except:
-    pass
+__version__ = "1.0.1"
